d245d754e2b4a97e48d0a653cace1cce146dee02
直希 <pg.stg.grnd@gmail.com>
2017-03-20 00:22:40 +0900

    Refactoring.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,17 +1,15 @@
 class BonusDrink
-  SERVICE_REQUIRED_BOTTLES = 3
+  SERVICE_REQUIRED_BOTTLES = 3  # サービスを受けるために必要な瓶の数
 
   def self.total_count_for(amount)
     sho, amari = amount.divmod(SERVICE_REQUIRED_BOTTLES)
-    returned_bottles = sho * SERVICE_REQUIRED_BOTTLES
-    existing_bottles = sho + amari
+    returned_bottles = sho * SERVICE_REQUIRED_BOTTLES     # サービスを受けるために返却した瓶
+    existing_bottles = sho + amari                        # 手元にある瓶
 
-    if existing_bottles > (SERVICE_REQUIRED_BOTTLES - 1)
-      service_bottles = total_count_for(existing_bottles)
-    else
-      service_bottles = existing_bottles
+    if existing_bottles >= SERVICE_REQUIRED_BOTTLES
+      existing_bottles = total_count_for(existing_bottles)
     end
 
-    returned_bottles + service_bottles
+    returned_bottles + existing_bottles
   end
 end
